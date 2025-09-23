1d865a3f4c2be33c9b08fccfb0cfe075cebc2d42
Nektarios Christou <nekcht@gmail.com>
2025-09-23 14:36:19 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from PyQt6.QtWidgets import QApplication
+from gui.main_window import MainWindow
+import sys
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
